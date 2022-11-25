96592337b5beda2c6a628dda41dcf3388ae703d8
hocnvph27417 <hocnvph27417@fpt.edu.vn>
2022-11-25 22:05:47 +0700

    update code

---
+++ seminar_hibernate-master/src/main/java/com/mycompany/hibernate/hibernateDemo/HibernatUtil.java
@@ -0,0 +1,47 @@
+package com.mycompany.hibernate.hibernateDemo;
+
+import com.mycompany.hibernate.model.SinhVien;
+import java.util.Properties;
+import com.mycompany.hibernate.model.LopHoc;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernatUtil {
+
+    private static final SessionFactory FACTORY;
+
+    static {
+        Configuration conf = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=hibernateDemo2");
+        properties.put(Environment.USER, "sa");
+        properties.put(Environment.PASS, "123");
+        properties.put(Environment.SHOW_SQL, "true");
+        //gen DB tự động
+        properties.put(Environment.HBM2DDL_AUTO, "create");
+
+        conf.setProperties(properties);
+        conf.addAnnotatedClass(LopHoc.class);
+        conf.addAnnotatedClass(SinhVien.class);
+
+        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+
+    }
+
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        getFACTORY();
+    }
+
+}
